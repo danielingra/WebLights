3fd0b333a5b8d3144b211cbe17b69719194724f1
danielingra <61312653+danielingra@users.noreply.github.com>
2021-04-17 14:59:49 +0200

    added previous higlight selection

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-function nextHighlight(){
+function setHighlight(orderType){
     //  Array with elements list, new and old selected children
-    var elementResponse = findElement();
+    var elementResponse = findElement(orderType);
     //  check if old selected children exists
     //  ...if so, remove selected attribute and background
     if (elementResponse["elementList"][elementResponse["oldSelected"]])
     {
         var oldSelected = elementResponse["elementList"][elementResponse["oldSelected"]];
         oldSelected.removeAttribute("selected");
-        oldSelected.style.background = "blue";
+        oldSelected.classList.remove("newSelected");
     }
     //  add selected attribute and background color
     //  ...to the new selected children
     var newSelected = elementResponse["elementList"][elementResponse["newSelected"]];
     newSelected.setAttribute("selected","true");
-    newSelected.style.background = "red";
-    console.log(elementResponse);   
+    newSelected.classList.add("newSelected");
+
+    //  add background layer to focus selected children
+    addLayer();
 }
 
-function findElement(){
+function addLayer(){
+
+    if (!document.getElementById("newLayer")){
+        newLayer = document.createElement("div");
+        newLayer.id = "newLayer";
+        document.body.appendChild(newLayer);
+    }
+}
+
+function findElement(orderType){
     var elementType = document.getElementsByTagName("p");
     var elementList = Array.prototype.slice.call(elementType);
     var response = [];
@@ -27,17 +38,33 @@ function findElement(){
     //  find and return old and new selected
     for (let index = 0; index < elementList.length; index++) {
         const element = elementList[index];
-        if (element.hasAttribute("selected")){
+        if (element.hasAttribute("selected")) {
+            //  oldSelected 
             response["oldSelected"] = index ;
-            response["newSelected"] = index + 1;
-            //  back to the first chiildren
-            if (index == (elementList.length - 1) ){
-                response["newSelected"] = 0;
+            //  define newSelected based on orderType(previous,next)
+            switch (orderType) {
+                case 'previous': 
+                {
+                    response["newSelected"] = index - 1;
+                    //  back to the last chiildren
+                    if (index == 0 ){
+                        response["newSelected"] = (elementList.length - 1) ;
+                    }
+                    break;
+                }
+                case 'next': 
+                {
+                    response["newSelected"] = index + 1;            
+                    //  back to the first chiildren
+                    if (index == (elementList.length - 1) ){
+                        response["newSelected"] = 0;
+                    }
+                    break;
+                }
             }
             return response;
         }       
     }
-
     //  ...there's no selection already
     //  ...so the newSelected is the firt child
     response["newSelected"] = 0;
